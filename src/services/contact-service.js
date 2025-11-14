8d3f39064ade394d6707d7f5b069787dd99e549d
hilmiyahya19 <hilmiyahya199@gmail.com>
2025-11-14 20:03:14 +0700

    Get Contact API

--- src/services/contact-service.js
+++ src/services/contact-service.js
@@ -1,5 +1,6 @@
 import { prismaClient } from "../application/database.js";
-import { createContactValidation } from "../validations/contact-validation.js";
+import { ResponseError } from "../error/response-error.js";
+import { createContactValidation, getContactValidation } from "../validations/contact-validation.js";
 import { validate } from "../validations/validation.js";
 
 const create = async (user, request) => {
@@ -18,6 +19,32 @@ const create = async (user, request) => {
     });
 }
 
+const get = async (user, contactId) => {
+    contactId = validate(getContactValidation, contactId);
+
+    const foundContact = await prismaClient.contact.findFirst({
+        where: {
+            // jangan sampai mengambil data contact milik user lain
+            username: user.username,
+            id: contactId
+        },
+        select: {
+            id: true,
+            first_name: true,
+            last_name: true,
+            email: true,
+            phone: true
+        }
+    });
+
+    if (!foundContact) {
+        throw new ResponseError(404, "Contact not found");
+    }
+
+    return foundContact;
+}
+
 export default {
-    create
+    create,
+    get
 }
--- tests/contact.test.js
+++ tests/contact.test.js
@@ -1,8 +1,11 @@
 import supertest from "supertest";
 import { logger } from "../src/application/logging.js";
 import { web } from "../src/application/web.js";
-import { createTestUser, removeAllTestContacts, removeTestUser } from "./test-util.js";
+import { createTestContact, createTestUser, getTestContact, removeAllTestContacts, removeTestUser } from "./test-util.js";
 
+// ================================================
+// TEST POST CONTACT
+// ================================================
 describe ('POST /api/contacts', () => {
     beforeEach(async () => {
         await createTestUser();
@@ -24,7 +27,7 @@ describe ('POST /api/contacts', () => {
                 phone: '081234567890'
         });
 
-        logger.info(response.body);
+        // logger.info(response.body);
 
         expect(response.status).toBe(201);
         expect(response.body.data.id).toBeDefined();
@@ -45,9 +48,54 @@ describe ('POST /api/contacts', () => {
                 phone: '0812345678901234567890'
         });
 
-        logger.info(response.body);
+        // logger.info(response.body);
 
         expect(response.status).toBe(400);
         expect(response.body.errors).toBeDefined();
     });
 });
+
+// ================================================
+// TEST GET CONTACT
+// ================================================
+describe ('GET /api/contacts/:contactId', () => {
+    beforeEach(async () => {
+        await createTestUser();
+        await createTestContact();
+    })
+
+    afterEach(async () => {
+        await removeAllTestContacts();
+        await removeTestUser();
+    });
+
+    it('should can get contact by id', async () => {
+        const testContact = await getTestContact();
+
+        const response = await supertest(web)
+            .get('/api/contacts/' + testContact.id)
+            .set('Authorization', 'test');
+
+        // logger.info(response.body);
+
+        expect(response.status).toBe(200);
+        expect(response.body.data.id).toBe(testContact.id);
+        expect(response.body.data.first_name).toBe(testContact.first_name);
+        expect(response.body.data.last_name).toBe(testContact.last_name);
+        expect(response.body.data.email).toBe(testContact.email);
+        expect(response.body.data.phone).toBe(testContact.phone);  
+    });
+
+    it('should return 404 if contact id not found', async () => {
+        const testContact = await getTestContact();
+
+        const response = await supertest(web)
+            .get('/api/contacts/' + testContact.id + 1)
+            .set('Authorization', 'test');
+
+        // logger.info(response.body);
+
+        expect(response.status).toBe(404);
+        expect(response.body.errors).toBeDefined();
+    });
+})
